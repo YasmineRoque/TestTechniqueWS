f0a5269c95bf90ecb4f059599f659e190cd9d251
Yaz <guemanzii@gmail.com>
2022-11-10 13:14:03 +0100

    créer l'app avec React et installer Bulma et sass

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ReadMember from "./ReadMember";
+
+function App() {
+  return (
+    <div className="App">
+      <ReadMember />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/ReadMember.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Fragment } from "react";
+
+const ReadMember = () => {
+  return (
+    <Fragment>
+      <section class="hero is-primary">
+        <div class="hero-body">
+          <p class="title">Primary hero</p>
+          <p class="subtitle">Primary subtitle</p>
+        </div>
+      </section>
+    </Fragment>
+  );
+};
+export default ReadMember;
