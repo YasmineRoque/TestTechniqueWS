a11d5a07489bdf8714d3bc4c9d8046489999eb2b
Yaz <guemanzii@gmail.com>
2022-11-10 22:50:04 +0100

    L'ajout de ReadMember

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import "bulma/css/bulma.min.css";
 import CreateMember from "./CreateMember";
 import Hero from "./Hero";
+import ReadMember from "./ReadMember";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Hero />
       <h1 className="title is-2 has-text-centered">Ajouter un(e) Argonaute</h1>
       <CreateMember />
+      <ReadMember />
     </div>
   );
 }
--- frontend/src/ReadMember.js
+++ frontend/src/ReadMember.js
@@ -2,6 +2,26 @@ import React from "react";
 import { Fragment } from "react";
 
 const ReadMember = () => {
-  return <Fragment></Fragment>;
+  const argonautes = [
+    { name: "Hello" },
+    { name: "yaz" },
+    { name: "Gilou" },
+    { name: "Joao" },
+    { name: "Nelia" },
+  ];
+  return (
+    <Fragment>
+      <div class="columns is-multiline">
+        {argonautes.map((add) => {
+          return (
+            <div key={add} class="column is-3">
+              <h3 className="title has-text-centered is-size-4">{add.name}</h3>
+            </div>
+          );
+        })}
+        ;
+      </div>
+    </Fragment>
+  );
 };
 export default ReadMember;
